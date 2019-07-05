0efa1746c711f7efd7cce1dd4a9fb1af0773b09d
Eduardo Canche <eduardo.942712@gmail.com>
2019-07-04 20:30:15 -0500

    added username in the user model

--- controllers/user.js
+++ controllers/user.js
@@ -5,12 +5,15 @@ const jwt = require('jsonwebtoken');
 exports.createUser = (req, res, next) => {
 	bcrypt.hash(req.body.password, 10)
 		.then((hash) => {
-			const user = new User({ email: req.body.email, password: hash });
+			const user = new User({ email: req.body.email, password: hash, username: req.body.username });
 			user.save()
 				.then((result) => {
 					return res.status(200).json({
 						message: 'User created',
-						result: result
+						result: {
+							...result.toObject(),
+							id: result._id
+						}
 					});
 				})
 				.catch(err => {
